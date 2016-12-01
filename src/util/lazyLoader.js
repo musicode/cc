40c27a0b56c3dbf208255203e95e7cc1dca4e3ba
musicode <musicode@qq.com>
2016-12-01 15:05:01 +0800

    update

--- src/util/lazyLoader.js
+++ src/util/lazyLoader.js
@@ -1,5 +1,5 @@
 /**
- * @file 图片懒加载
+ * @file 图片懒加载，支持背景图懒加载
  * @author musicode
  */
 define(function (require, exports, module) {
@@ -34,38 +34,60 @@ define(function (require, exports, module) {
         return '';
     };
 
-    exports.load = function (container, selector) {
+    /**
+     * 初始化懒加载
+     *
+     * @param {jQuery} container
+     * @param {string} selector
+     * @param {?string} loadedClass 如果是背景图懒加载，需标记已加载的 class
+     */
+    exports.load = function (container, selector, loadedClass) {
 
         var images = container.find(selector || 'img');
         var namespace = '.' + guid();
 
+        var isLoaded = loadedClass
+            ? function (element) {
+                return element.hasClass(loadedClass);
+            }
+            : function (element) {
+                return element.prop('src');
+            };
+
+        var setImage = loadedClass
+            ? function (element) {
+                element.addClass(loadedClass);
+            }
+            : function (element) {
+                var data = element.data();
+                var url1x = exports.getImageUrl({
+                    src: data.src,
+                    width: data.width,
+                    height: data.height
+                });
+                var url2x = exports.getImageUrl({
+                    src: data.src,
+                    width: data.width * 2,
+                    height: data.height * 2
+                });
+                var props = {
+                    src: url1x
+                };
+                if (url2x !== url1x) {
+                    props.srcset = url2x + ' 2x';
+                }
+                element.prop(props);
+            };
+
         var loadImage = function () {
             var loadComplete = true;
             var scrollBottom = pageScrollTop() + viewportHeight();
-
             images.each(function () {
                 var element = $(this);
-                if (!element.prop('src')) {
+                if (!isLoaded(element)) {
                     var imageTop = element.offset().top;
                     if (imageTop - exports.threshold < scrollBottom) {
-                        var data = element.data();
-                        var url1x = exports.getImageUrl({
-                            src: data.src,
-                            width: data.width,
-                            height: data.height
-                        });
-                        var url2x = exports.getImageUrl({
-                            src: data.src,
-                            width: data.width * 2,
-                            height: data.height * 2
-                        });
-                        var props = {
-                            src: url1x
-                        };
-                        if (url2x !== url1x) {
-                            props.srcset = url2x + ' 2x';
-                        }
-                        element.prop(props);
+                        setImage(element);
                         return;
                     }
                     if (loadComplete) {
